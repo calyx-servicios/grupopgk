2e449094c2b2137e06413c7d13c70735c7af8034
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-01-03 08:28:59 -0300

    [ODOO-5564][FIX][l10n_ar_afipws] - Change decodestring to decodebytes

---
+++ l10n_ar_afipws_decode/wizard/upload_certificate_wizard.py
@@ -0,0 +1,12 @@
+from odoo import fields, api, models
+import base64
+
+
+class L10nArAfipwsUploadCertificate(models.TransientModel):
+    _inherit = "afipws.upload_certificate.wizard"
+    
+    def action_confirm(self):
+        self.ensure_one()
+        self.certificate_id.write({"crt": base64.decodebytes(self.certificate_file)})
+        self.certificate_id.action_confirm()
+        return True
