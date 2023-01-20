48f6b4b9452bfd4673df89551ba020391253c246
Gabriela Perez <gabbiiperez94@gmail.com>
2023-01-20 18:46:27 -0300

    [ODOO-5767-5773-5780][IMP][timesheet_odoo] - IMP new models.

--- timesheet_odoo/__manifest__.py
+++ timesheet_odoo/__manifest__.py
@@ -8,14 +8,16 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Timesheet",
-    "version": "15.0.3.0.1",
+    "version": "15.0.4.0.2",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
     "depends": ['base', 'hr_timesheet', 'calendar_view_timesheet'],
     "data": [
+        "security/timesheet_sige_security.xml",
         "security/ir.model.access.csv",
-        "data/ir_cron.xml",
+        "data/mail_template_data.xml",
         "views/timesheet_sige_views.xml",
+        "views/period_sige_views.xml",
     ],
 }
