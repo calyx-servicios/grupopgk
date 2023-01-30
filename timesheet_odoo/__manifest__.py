09cab9658da90d4b9ba13b33f6c40f2af46ec5c6
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-01-30 15:01:13 -0300

    [ODOO-5868][fix][calendar_view_timesheet, timesheet_odoo] - Upgrade version

--- timesheet_odoo/__manifest__.py
+++ timesheet_odoo/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Timesheet",
-    "version": "15.0.6.0.0",
+    "version": "15.0.6.1.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
