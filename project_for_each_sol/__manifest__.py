117da7f19f4dc76fc7944b1de7515e5537b50237
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-02-10 14:42:12 -0300

    [ODOO-5953][UPD][project_for_each_sol, sale_order_locations_fields] -  Update to stg

--- project_for_each_sol/__manifest__.py
+++ project_for_each_sol/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "15.0.4.3.0",
+    "version": "15.0.5.0.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
