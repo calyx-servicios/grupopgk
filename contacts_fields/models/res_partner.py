15c951c640422a13ee3515b8a3a18cd6d287b490
Gabriela Perez <gabbiiperez94@gmail.com>
2023-02-17 12:28:34 -0300

    [ODOO-6003][FIX][contacts_fields] - FIX is company parent.

--- contacts_fields/models/res_partner.py
+++ contacts_fields/models/res_partner.py
@@ -1,9 +1,8 @@
 from odoo import fields, models
 
+
 class Partner(models.Model):
     _inherit = "res.partner"
-    
-    vat = fields.Char(string='VAT', index=True, 
-        help="The Tax Identification Number. Complete it if the contact is subjected to government taxes. Used in some legal statements.",
-        required=True, copy=False, tracking=5)
+
+    is_parent = fields.Boolean("Is company parent", default=False)
 
