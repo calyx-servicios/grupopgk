73731cdc8ed9cf36e51549e639d8d689884056d8
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-02-17 13:16:46 -0300

    [ODOO-6042][UPD][journal_field_without_domain] - Update to PROD

---
+++ journal_field_without_domain/__manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Journal Field Without Domain",
+    "summary": """
+        This module removes the filter in the journal in the Journal Entries.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["Zamora. Javier"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Technical Settings",
+    "version": "15.0.1.0.0",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+
+}
