2645c24bd6e84267a2138b39b9a4b8c1ce18ddeb
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-02-22 10:58:44 -0300

    [ODOO-6046][UPD][show_analytic_accounts] - Update PROD

---
+++ show_analytic_accounts/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    "name": "Show Analytical Accounts",
+    "summary": """
+        This module allow show analytical accounts with the check enabled.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["Zamora Javier"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Technical Settings",
+    "version": "15.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "analytic",
+        "sol_analytic_account",
+    ],
+    "data": [
+        "views/sale_order.xml",
+        "views/account_analytic_account.xml",
+    ],
+
+}
