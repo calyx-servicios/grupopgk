8404607d168ea39250311afbea5c121fd945de5a
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-02-27 11:12:22 -0300

    [ODOO-6046][UPD][show_analytic_accounts] - Upd to PROD

--- show_analytic_accounts/__manifest__.py
+++ show_analytic_accounts/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.1.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
