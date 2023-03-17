81849a77b65104defcf3a547e0857ec6c2e02814
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-03-17 16:04:52 -0300

    [ODOO-6161][UPD][timesheet_odoo] - Update on stg

--- timesheet_odoo/models/account_analytic_line.py
+++ timesheet_odoo/models/account_analytic_line.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
-    timesheet_id = fields.Many2one("timesheet.sige", string="Timesheet")
+    timesheet_id = fields.Many2one("timesheet.sige", string="Timesheet", ondelete="cascade")
