beab69d58f23aa44ed2d6460b68bab6efd76e96f
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-03-31 18:15:20 -0300

    [ODOO-6188][UPD][account_analytic_currency] - Upd to STG

---
+++ account_analytic_currency/__manifest__.py
@@ -0,0 +1,24 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Analytic Currency",
+    "summary": """
+        This module show on analytic line the currency from journal entry.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["Zamora, Javier"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Technical Settings",
+    "version": "15.0.1.2.0",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "analytic",
+        "account",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/account_analytic_line.xml",
+        "wizards/update_currency.xml",
+    ],
+}
