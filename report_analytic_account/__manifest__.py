b8cc95e60e8af48020e13387def6f89d64e1ba1b
garaceliguzman <georginaguzman267@gmail.com>
2023-03-31 16:56:52 -0300

    [ODOO-6225][ADD][report_analytic_account]

---
+++ report_analytic_account/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Report Analytic Account",
+    "summary": """
+       Report Analytic Account""",
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["GeorginaGuzman"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Account",
+    "version": "15.0.1.0.3",
+    "application": False,
+    "installable": True,
+    "depends": [ 'account', 'analytic'],
+    "data": [
+        "views/analityc_report.xml"
+    ],
+}
