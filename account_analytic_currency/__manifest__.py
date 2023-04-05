0d84f13086df91e74c86808f1e90972506849b44
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-04-05 08:54:27 -0300

    [ODOO-6276][FIX][account_analytic_currency] - Fix Version

--- account_analytic_currency/__manifest__.py
+++ account_analytic_currency/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "15.0.1.2.0",
+    "version": "15.0.2.0.0",
     "application": False,
     "installable": True,
     "depends": [
