2170c6c676e16709f9bce9758f1d0f7d473c2023
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-04-13 17:32:13 -0300

    [ODOO-6352][UPD][reṕort_analytic_account] - update on Stg

--- report_analytic_account/models/account_analytic_line.py
+++ report_analytic_account/models/account_analytic_line.py
@@ -3,16 +3,9 @@ from odoo.osv import expression
 from collections import defaultdict
 from odoo.exceptions import ValidationError
 
-
-class AccountAnalyticGroup(models.Model):
-    _inherit = 'account.analytic.group'
-
-    parent_prin_group = fields.Many2one('account.analytic.group', string="Parent prin", ondelete='cascade', domain="['|', ('company_id', '=', False), ('company_id', '=', company_id)]")
-
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
-    parent_prin_group_id = fields.Many2one('account.analytic.group', related='group_id.parent_prin_group', store=True, readonly=True, compute_sudo=True)
     group_parent_id = fields.Many2one('account.analytic.group', related='group_id.parent_id', store=True, readonly=True, compute_sudo=True)
     account_mother_id = fields.Many2one('account.analytic.account', related='account_id.parent_id', store=True, readonly=True, compute_sudo=True)
     balance = fields.Monetary(related='account_id.balance', string='Balance',  groups='account.group_account_readonly')
