848623304e8e280c42be9617d4213aafcd5f58ee
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-04-25 16:58:18 -0300

    [ODOO-6225][UPD][sale_subscription_custo_Field] - Update on stg

---
+++ sale_subscription_custom_fields/__manifest__.py
@@ -0,0 +1,24 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Subscription Fields",
+    "summary": """
+        This module transfers key fields from Sales Orders to Subscriptions and Invoices, improving subscription management and saving time.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["Zamora, Javier"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Sign",
+    "version": "15.0.1.2.1",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'subscription_templates',
+        'sale_order_subscription',
+        'sale_order_split_invoices',
+        'subscripcion_force_invoice',
+    ],
+    "data": [
+        'views/subscription_package.xml',
+    ],
+}
---
+++ sale_subscription_custom_fields/models/sale_order.py
@@ -0,0 +1,53 @@
+from odoo import  models, _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    def _prepare_subscription(self, plan, split_line):
+        res = super(SaleOrder, self)._prepare_subscription(plan, split_line)
+        res['payment_term_id'] = self.payment_term_id.id
+        return res
+
+    def _prepare_subscription_data(self, plan):
+        res = super(SaleOrder, self)._prepare_subscription_data(plan)
+        res['payment_term_id'] = self.payment_term_id.id
+        return res
+
+    def _prepare_subscription_lines(self, split_line):
+        values = []
+        for lines in split_line:
+            if not lines.product_id.is_dues_ok:
+                price = lines.order_id.set_amount(lines)
+                values.append(((0, False, {
+                    'sequence': lines.sequence,
+                    'product_id': lines.product_id.id,
+                    'name_product': lines.name,
+                    'analytic_account_id': lines.analytic_account_id.id,
+                    'product_qty': lines.quantity,
+                    'product_uom_id': lines.uom_id.id,
+                    'unit_price': price,
+                    'tax_id': [(6, 0, lines.tax_id.ids)],
+                })))
+        return values
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+    
+    
+    def _prepare_values_product(self):
+        values = []
+        for line in self:
+            if not line.product_id.is_dues_ok:
+                price = line.price_unit
+                values.append((0, False, {
+                    'sequence': line.sequence,
+                    'product_id': line.product_id.id,
+                    'name_product': line.name,
+                    'analytic_account_id': line.analytic_account_id.id,
+                    'product_qty': line.product_uom_qty,
+                    'product_uom_id': line.product_uom.id,
+                    'unit_price': price,
+                    'tax_id': [(6, 0, line.tax_id.ids)],
+                }))
+        return values
