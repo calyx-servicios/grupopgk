274799d511f1bf1f6805289c0767c937e84767ef
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-05-10 11:10:39 -0300

    [ODOO-6528][FIX][consolidation_report, report_analytic_account] - Group excel, select al record in period,etc

--- consolidation_report/models/account_consolidation_company.py
+++ consolidation_report/models/account_consolidation_company.py
@@ -5,7 +5,7 @@ class AccountConsolidationCompany(models.Model):
     _name = 'account.consolidation.company'
     _description = 'Companies and currencies for consolidation report'
 
-    account_consolidation_report_id = fields.Many2one('account.consolidation.report', string='Consolidation Report')
+    account_consolidation_report_id = fields.Many2one('account.consolidation.period', string='Consolidation Period')
     company_id = fields.Many2one('res.company', string='Company')
     currency_id = fields.Many2one('res.currency', string='Currency Origin')
     new_currency = fields.Many2one('res.currency', string='New Currency')
