6dc2c2ed00fa5abadb59e259754d1bd9f05535a9
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-05-15 15:31:02 -0300

    [ODOO-6525][UPD][consolidation_report] - Update on stg

--- consolidation_report/models/account_consolidation_report.py
+++ consolidation_report/models/account_consolidation_report.py
@@ -34,8 +34,10 @@ class AccountConsolidationReport(models.Model):
     def compute_consolidation_data(self):
         for record in self:
             data = {}
+            totals = {}
             if record.consolidation_period.consolidation_companies:
                 data = record.prepare_excel_data()
+                totals = record.get_totals(data)
                 # Create Excel file
                 output = BytesIO()
                 workbook = xlsxwriter.Workbook(output)
@@ -80,31 +82,35 @@ class AccountConsolidationReport(models.Model):
                 for grandfather_group, mother_groups in data.items():
                     for mother_group, grandmother_accounts in mother_groups.items():
                         for grandmother_account, mother_accounts in grandmother_accounts.items():
-                            for mother_account, daughter_accounts in mother_accounts.items():
-                                for daughter_account, vals in daughter_accounts.items():
-                                    worksheet.merge_range(row, 0, row, 7, grandfather_group, merge_format)
-                                    row += 1
-                                    worksheet.merge_range(row, 0, row, 7, grandfather_group + ' / ' + mother_group, merge_format)
-                                    row += 1
-                                    worksheet.merge_range(row, 0, row, 7, grandfather_group + ' / ' + mother_group + ' / ' + grandmother_account, merge_format)
-                                    row += 1
-                                    worksheet.merge_range(row, 0, row, 7, grandfather_group + ' / ' + mother_group + ' / ' + grandmother_account + ' / ' + mother_account, merge_format)
-                                    row += 1
-                                    worksheet.merge_range(row, 0, row, 7, grandfather_group + ' / ' + mother_group + ' / ' + grandmother_account + ' / ' + mother_account + '/' + daughter_account, merge_format)
-                                    row += 1
-                                    total = 0
-                                    for val in vals:
-                                        worksheet.write(row, 0, val['description'])
-                                        worksheet.write(row, 1, val['account_id'])
-                                        worksheet.write(row, 2, val['company'])
-                                        worksheet.write(row, 3, val['currency_origin'])
-                                        worksheet.write(row, 4, val['currency'])
-                                        worksheet.write(row, 5, val['rate'])
-                                        worksheet.write(row, 6, val['amount'])
-                                        total += val['amount']
+                            for mother_account, companies in mother_accounts.items():
+                                for company, daughter_accounts in companies.items():
+                                    for daughter_account, vals in daughter_accounts.items():
+                                        worksheet.merge_range(row, 0, row, 6, grandfather_group, merge_format)
+                                        worksheet.write(row, 7, totals[grandfather_group]['total'], merge_format)
                                         row += 1
-                                    worksheet.write(row, 7, total)
-                                    row += 1
+                                        worksheet.merge_range(row, 0, row, 6, grandfather_group + ' / ' + mother_group, merge_format)
+                                        worksheet.write(row, 7, totals[grandfather_group][mother_group]['total'], merge_format)
+                                        row += 1
+                                        worksheet.merge_range(row, 0, row, 6, grandfather_group + ' / ' + mother_group + ' / ' + grandmother_account, merge_format)
+                                        worksheet.write(row, 7, totals[grandfather_group][mother_group][grandmother_account]['total'], merge_format)
+                                        row += 1
+                                        worksheet.merge_range(row, 0, row, 6, grandfather_group + ' / ' + mother_group + ' / ' + grandmother_account + ' / ' + mother_account, merge_format)
+                                        worksheet.write(row, 7, totals[grandfather_group][mother_group][grandmother_account][mother_account]['total'], merge_format)
+                                        row += 1
+                                        worksheet.merge_range(row, 0, row, 6, grandfather_group + ' / ' + mother_group + ' / ' + grandmother_account + ' / ' + mother_account + '/' + company, merge_format)
+                                        worksheet.write(row, 7, totals[grandfather_group][mother_group][grandmother_account][mother_account][company]['total'], merge_format)
+                                        row += 1
+                                        worksheet.merge_range(row, 0, row, 6, grandfather_group + ' / ' + mother_group + ' / ' + grandmother_account + ' / ' + mother_account + '/' + company + '/' + daughter_account, merge_format)
+                                        row += 1
+                                        for val in vals:
+                                            worksheet.write(row, 0, val['description'])
+                                            worksheet.write(row, 1, val['account_id'])
+                                            worksheet.write(row, 2, val['company'])
+                                            worksheet.write(row, 3, val['currency_origin'])
+                                            worksheet.write(row, 4, val['currency'])
+                                            worksheet.write(row, 5, val['rate'])
+                                            worksheet.write(row, 6, val['amount'])
+                                            row += 1
 
                 # Save and encode file
                 workbook.close()
@@ -116,60 +122,43 @@ class AccountConsolidationReport(models.Model):
                 record.export_consolidation_data = encoded_file
 
     def prepare_excel_data(self):
+        data = {}
 
-        for record in self:
-            data = {}
+        analytic_lines = self.env['account.analytic.line'].search([
+            ('date', '>=', self.consolidation_period.date_from),
+            ('date', '<=', self.consolidation_period.date_to)
+        ])
 
-            if record.consolidation_period.consolidation_companies:
-                grandfather_groups = self.env['account.analytic.group'].search([('parent_id', '=', False)])
-                domain = [('date', '>=', record.consolidation_period.date_from), ('date', '<=', record.consolidation_period.date_to)]
-                analytic_lines = self.env['account.analytic.line'].search(domain)
-                if analytic_lines:
-                    for line in analytic_lines:
-                        grandfather_group = line.account_id.group_id.parent_id.name or 'Undefined'
-                        mother_group = line.account_id.group_id.name or 'Undefined'
-                        grandmother_account = line.account_id.parent_id.parent_id.name or 'Undefined'
-                        mother_account = line.account_id.parent_id.name or 'Undefined'
-                        daughter_account = line.account_id.name or 'Undefined'
+        for analytic_line in analytic_lines:
+            analytic_line.update_currency_id()
 
-                        if grandfather_group not in data:
-                            data[grandfather_group] = {}
-                        if mother_group not in data[grandfather_group]:
-                            data[grandfather_group][mother_group] = {}
-                        if grandmother_account not in data[grandfather_group][mother_group]:
-                            data[grandfather_group][mother_group][grandmother_account] = {}
-                        if mother_account not in data[grandfather_group][mother_group][grandmother_account]:
-                            data[grandfather_group][mother_group][grandmother_account][mother_account] = {}
+            group_key = analytic_line.account_id.group_id.parent_id.name or 'Undefined'
+            mother_key = analytic_line.account_id.group_id.name or 'Undefined'
+            grandmother_key = analytic_line.account_id.parent_id.parent_id.name or 'Undefined'
+            mother_account_key = analytic_line.account_id.parent_id.name or 'Undefined'
+            daughter_account_key = analytic_line.account_id.name or 'Undefined'
+            company = analytic_line.move_id.company_id.name or 'Undefined'
 
-                        # _logger.info('witchcraft: {} - '.format(data))
-                        rate = None
-                        new_currency = None
-                        currency_origin = None
-                        if line.move_id.company_id in record.consolidation_period.consolidation_companies.mapped('company_id'):
-                            consolidation_period = record.consolidation_period.consolidation_companies.filtered(lambda x: x.company_id == line.move_id.company_id)
-                            if consolidation_period:
-                                currency_origin = consolidation_period.currency_id.symbol
-                                new_currency = consolidation_period.new_currency.symbol
-                                if consolidation_period.historical_rate:
-                                    if line.move_id.move_id.l10n_ar_currency_rate:
-                                        rate = line.move_id.move_id.l10n_ar_currency_rate
-                                    else:
-                                        date = line.move_id.date
-                                        rate = line.move_id.currency_id.with_context(date=date).rate
-                                else:
-                                    rate = consolidation_period.rate
+            consolidation_period = self.consolidation_period.consolidation_companies.filtered(lambda x: x.company_id == analytic_line.move_id.company_id)
+            currency_origin = consolidation_period.currency_id.symbol if consolidation_period else analytic_line.currency_id.symbol
+            new_currency = consolidation_period.new_currency.symbol if consolidation_period else analytic_line.currency_id.symbol
+            rate = consolidation_period.rate if consolidation_period and not consolidation_period.historical_rate else 1
 
-                        data[grandfather_group][mother_group][grandmother_account][mother_account].setdefault(daughter_account, []).append({
-                            'account_id': line.account_id.code,
-                            'company': line.move_id.company_id.name,
-                            'currency_origin': currency_origin if currency_origin else'',
-                            'currency': new_currency if new_currency else '',
-                            'rate': rate if rate else 1,
-                            'description': line.name or '',
-                            'amount': line.amount * rate if rate else line.amount,
-                        })
+            daughter_account = data.setdefault(group_key, {}).setdefault(mother_key, {}).setdefault(grandmother_key, {}).setdefault(mother_account_key, {}).setdefault(company, {}).setdefault(daughter_account_key, [])
 
-            return data
+            daughter_account.append({
+                'account_id': analytic_line.general_account_id.code,
+                'company': company,
+                'currency_origin': currency_origin if currency_origin else '',
+                'currency': new_currency if new_currency else '',
+                'rate': rate,
+                'description': analytic_line.name or '',
+                'amount': analytic_line.amount * rate if not consolidation_period or not consolidation_period.historical_rate else analytic_line.amount,
+            })
+
+        return data
+
+
 
     @api.depends('export_consolidation_data', 'period')
     def _compute_files(self):
@@ -180,3 +169,70 @@ class AccountConsolidationReport(models.Model):
                 record.export_consolidation_file = record.export_consolidation_data
             else:
                 record.export_consolidation_file = False
+
+    def get_totals(self, data):
+        totals = {}
+
+        for group_key, group_value in data.items():
+            group_total = 0
+            group_dict = {'total': group_total}
+
+            for mother_key, mother_value in group_value.items():
+                mother_dict = self.calculate_mother_totals(mother_value)
+                group_total += mother_dict['total']
+                group_dict[mother_key] = mother_dict
+
+            group_dict['total'] = group_total
+            totals[group_key] = group_dict
+
+        return totals
+
+    def calculate_mother_totals(self, mother_value):
+        mother_total = 0
+        mother_dict = {'total': mother_total}
+
+        for grandmother_key, grandmother_value in mother_value.items():
+            grandmother_dict = self.calculate_grandmother_totals(grandmother_value)
+            mother_total += grandmother_dict['total']
+            mother_dict[grandmother_key] = grandmother_dict
+
+        mother_dict['total'] = mother_total
+        return mother_dict
+
+    def calculate_grandmother_totals(self, grandmother_value):
+        grandmother_total = 0
+        grandmother_dict = {'total': grandmother_total}
+
+        for mother_account_key, mother_account_value in grandmother_value.items():
+            mother_account_dict = self.calculate_mother_account_totals(mother_account_value)
+            grandmother_total += mother_account_dict['total']
+            grandmother_dict[mother_account_key] = mother_account_dict
+
+        grandmother_dict['total'] = grandmother_total
+        return grandmother_dict
+
+    def calculate_mother_account_totals(self, mother_account_value):
+        mother_account_total = 0
+        mother_account_dict = {'total': mother_account_total}
+
+        for daughter_account_key, daughter_account_value in mother_account_value.items():
+            daughter_account_dict = self.calculate_daughter_account_totals(daughter_account_value)
+            mother_account_total += daughter_account_dict['total']
+            mother_account_dict[daughter_account_key] = daughter_account_dict
+
+        mother_account_dict['total'] = mother_account_total
+        return mother_account_dict
+
+    def calculate_daughter_account_totals(self, daughter_account_value):
+        daughter_account_total = 0
+        daughter_account_dict = {'total': daughter_account_total}
+
+        for company_key, company_value in daughter_account_value.items():
+            company_total = sum(entry['amount'] for entry in company_value)
+            daughter_account_total += company_total
+            company_dict = {'entry': {'total': company_total}}
+            daughter_account_dict[company_key] = company_dict
+
+        daughter_account_dict['total'] = daughter_account_total
+        return daughter_account_dict
+
