2791dada5164037281d31d0bb00bbc573c385b17
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-06-23 14:26:55 -0300

    [ODOO-6400][UPD][timesheet_odoo] - Update on stg

--- timesheet_odoo/models/account_analytic_line.py
+++ timesheet_odoo/models/account_analytic_line.py
@@ -1,7 +1,15 @@
-from odoo import fields, models
+from odoo import fields, models, api, _
+from odoo.exceptions import ValidationError
 
 
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
     timesheet_id = fields.Many2one("timesheet.sige", string="Timesheet", ondelete="cascade")
+
+    @api.constrains('timesheet_ids')
+    def _check_timesheet_line(self):
+        for timesheet in self:
+            for line in timesheet.timesheet_ids:
+                if line.unit_amount < 0.5:
+                    raise ValidationError(_("Hours should not be less than 0.5."))
