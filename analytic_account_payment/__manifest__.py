9d3a77567e0101a71d72f18698ca2ad344e1f997
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-06-26 11:55:18 -0300

    [ODOO-6735][UPD][analytic_account_payment] - Update on stg

---
+++ analytic_account_payment/__manifest__.py
@@ -0,0 +1,19 @@
+# pylint: disable=missing-module-docstring,pointless-statement
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Analytic Account Payment",
+    "summary": """
+        This module calculates the analytical accounts of the payment entries
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["Zamora, Javier"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Account",
+    "version": "15.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "depends": ["account_payment_group"],
+    "data": [],
+}
