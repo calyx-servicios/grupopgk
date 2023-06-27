a14d78703e11b77c8842298d728ab212cb26547e
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-06-27 09:37:51 -0300

    [ODOO-6786][IMP][purchase_order] - Add new module for purchase refactoring

---
+++ purchase_refactoring_indicator/__manifest__.py
@@ -0,0 +1,21 @@
+# pylint: disable=missing-module-docstring,pointless-statement
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Refactoring Indicator",
+    "summary": """
+        This module adds a refactoring indicator to purchase orders.
+    """,
+    "author": "Calyx Servicios S.A.",
+    "maintainers": ["Zamora, Javier"],
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Purchase",
+    "version": "15.0.1.0.0",
+    "development_status": "Production/Stable",
+    "application": False,
+    "installable": True,
+    "depends": ["custom_access_permissions"],
+    "data": [
+        "views/purchase_order.xml",
+    ],
+}
