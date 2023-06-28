71f8f98b3ac5891b6f3000be5719754913285b32
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-06-28 14:19:48 -0300

    [ODOO-6786][FIX][purchase_refactoring_indicator] - Change span and translation

--- purchase_refactoring_indicator/__manifest__.py
+++ purchase_refactoring_indicator/__manifest__.py
@@ -10,7 +10,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Purchase",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
