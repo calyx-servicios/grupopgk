42708944b150aced0026faf8803d0bec4088f6ae
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-07-04 15:59:07 -0300

    [ODOO-6836][UPD][custom_access_permission] - pdate on prod

--- custom_access_permissions/models/account_move.py
+++ custom_access_permissions/models/account_move.py
@@ -9,7 +9,7 @@ class AccountMove(models.Model):
         if not self.env.user.has_group('custom_access_permissions.group_profile_manager') and vals.get('move_type') not in ('out_refund', 'in_refund'):
             raise AccessError(_('You do not have access to create this type of move.'))
         return super().create(vals)
-    
+
     def action_post(self):
         for rec in self:
             if not self.env.user.has_group('custom_access_permissions.group_profile_administrator'):
@@ -21,3 +21,18 @@ class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
     move_type = fields.Selection(related='move_id.move_type', string='Move Type', readonly=True, store=True)
+
+    has_profile_admin = fields.Boolean(string="Has administrator profile?", compute="_compute_has_profile_access", store=False, default=False)
+    has_profile_manager = fields.Boolean(string="Has manager profile?", compute="_compute_has_profile_access", store=False, default=False)
+
+    @api.depends('user_id')
+    def _compute_has_profile_access(self):
+        group_names = {
+            'custom_access_permissions.group_profile_administrator': 'has_profile_admin',
+            'custom_access_permissions.group_profile_manager': 'has_profile_manager'
+        }
+        for project in self:
+            user = self.env.user
+            for group_name, field_name in group_names.items():
+                has_group = user.has_group(group_name)
+                setattr(project, field_name, has_group)
