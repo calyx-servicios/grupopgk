7b52ea9a05ba7f7975488fab439aa4b5e588193c
JaviZamoraDev <javier.zamora.dev@gmail.com>
2023-08-22 09:34:34 -0300

    [ODOO-6743][UPD][timesheet_odoo] - Update on prod

--- timesheet_odoo/__manifest__.py
+++ timesheet_odoo/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Timesheet",
-    "version": "15.0.6.4.0",
+    "version": "15.0.6.9.4",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
