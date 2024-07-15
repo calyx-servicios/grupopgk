8b0fae52fb914ac41f346dd3bec580d337e54213
Leandro090685 <Leandro090685@github.com>
2024-07-15 14:26:14 -0300

    [FEATURE/ODOO-7978][UPD][SHOW_ANALYTIC-ACCOUNTS]

--- show_analytic_accounts/__manifest__.py
+++ show_analytic_accounts/__manifest__.py
@@ -1,20 +1,21 @@
 {
     "name": "Show Analytical Accounts",
     "summary": """
-        This module allow show analytical accounts with the check enabled.
+        This module allows you to show analytical accounts with the check enabled and does not allow you to archive analytical accounts that have active associated subscriptions
     """,
     "author": "Calyx Servicios S.A.",
-    "maintainers": ["Zamora Javier"],
+    "maintainers": ["Zamora Javier", "leandro090685"],
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "15.0.1.1.0",
+    "version": "15.0.3.1.0",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
     "depends": [
         "analytic",
         "sol_analytic_account",
+        "subscription_package",
     ],
     "data": [
         "views/sale_order.xml",
