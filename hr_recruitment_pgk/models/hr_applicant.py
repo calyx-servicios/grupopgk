7abaadd63d1fc76992e85a8eac5171c6b8dd59f3
leandro090685 <lenife@hotmail.com>
2024-08-30 13:05:44 -0300

    [ODOO-8194-STG][UPD][HR_RECRUITMENT_PGK]

--- hr_recruitment_pgk/models/hr_applicant.py
+++ hr_recruitment_pgk/models/hr_applicant.py
@@ -5,6 +5,8 @@ class HrApplicant(models.Model):
     _inherit = "hr.applicant"
 
     vat = fields.Char(string='CUIL')
+
+    old_postulation = fields.Boolean(string='Postulación antigua')
     
     def create_employee_from_applicant(self):
         dict_act_window = super(HrApplicant, self).create_employee_from_applicant()
@@ -19,4 +21,19 @@ class HrApplicant(models.Model):
                     'default_identification_id': applicant.vat,
                 })
         return dict_act_window
+
+
+    @api.model
+    def create(self, vals):
+        # Verificar si el campo 'cuil' está en los valores
+        if 'vat' in vals:
+            # Buscar en la base de datos si ya existe un registro con ese 'cuil'
+            existing_applicant = self.with_context(active_test=False).search([('vat', '=', vals['vat'])], limit=1)
+            if existing_applicant:
+                # Agregar 'old_postulation' al vals
+                vals['old_postulation'] = True
+
+        # Crear el nuevo registro
+        res = super(HrApplicant, self).create(vals)
+        return res
     
