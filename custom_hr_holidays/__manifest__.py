7fbf596529d976580c8c04cb4f8a1d75e89da640
leandro090685 <lenife@hotmail.com>
2024-09-03 14:15:59 -0300

    [ODOO-8205-STG][UPD][CUSTOM_HR_HOLIDAYS]

--- custom_hr_holidays/__manifest__.py
+++ custom_hr_holidays/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "15.0.6.0.1",
+    "version": "15.0.7.0.1",
     "application": False,
     "installable": True,
     "depends": ['hr_holidays',
