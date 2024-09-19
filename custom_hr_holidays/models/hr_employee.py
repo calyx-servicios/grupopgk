08020b50ec8d594ebc1c2460b6f54c82add578a4
leandro090685 <lenife@hotmail.com>
2024-09-19 09:21:23 -0300

    [FEATURE/ODOO][UPD3][CUSTOM_HR_HOLIDAYS]

--- custom_hr_holidays/models/hr_employee.py
+++ custom_hr_holidays/models/hr_employee.py
@@ -40,7 +40,7 @@ class EmployeePublic(models.Model):
     _inherit = 'hr.employee.public'
 
     legajo = fields.Integer(string="Legajo")
-    partner = fields.Many2one('res.partner', string="Partner")
-    entry_date = fields.Date(string="Entry Date", required=True)
-    exit_date = fields.Date(string="Exit Date")
-    is_active = fields.Boolean('Active Employee?', compute='_compute_is_active', store=True, default=True)
+    vacation_days = fields.Integer(
+        string="Vacation Days",
+        store=True
+    )
