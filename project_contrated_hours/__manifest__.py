fadcf48e4549a759d8ec73546a3147bb8956c039
Esteban Suarez <estebansamsuarez20@gmail.com>
2025-02-07 12:32:24 -0300

    [UPD][project_contrated_hours]

--- project_contrated_hours/__manifest__.py
+++ project_contrated_hours/__manifest__.py
@@ -4,11 +4,11 @@
         This module add contrated hours from sale order lines
     """,
     "author": "Calyx Servicios S.A.",
-    "maintainers": ["AgusCFx"],
+    "maintainers": ["AgusCFx", "EstebanSuarez21"],
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "15.0.1.0.1",
+    "version": "15.0.2.0.1",
     "application": False,
     "installable": True,
     "depends": [
