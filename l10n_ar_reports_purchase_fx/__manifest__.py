c73e020ed34679c0f5feb1c89c1b1dad394ec068
Andrés Shoobridge <shoobridgea@gmail.com>
2025-09-11 17:34:27 -0300

    Corrección txt purchase_vat_book, para presentarlo en dolares ante ARCA

---
+++ l10n_ar_reports_purchase_fx/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "AR Reports - Purchase FX Fix",
+    "version": "15.0.1.0.0",
+    "category": "Localization/Argentina",
+    "sequence": 15,
+    "author": "ADHOC SA, Moldeo Interactive, OCA, Custom Fix",
+    "license": "AGPL-3",
+    "summary": "Fix amount logic for purchase invoices in foreign currency",
+    "depends": [
+        "l10n_ar_reports",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
