8909a57c819f7b6abd67c94ac101087669ce8e39
Esteban Suarez <estebansamsuarez20@gmail.com>
2025-10-24 16:10:46 -0300

    [FIX][project_contrated_hours]

--- project_contrated_hours/__manifest__.py
+++ project_contrated_hours/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "15.0.3.3.0",
+    "version": "15.0.3.3.1",
     "application": False,
     "installable": True,
     "depends": [
