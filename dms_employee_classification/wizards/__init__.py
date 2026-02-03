6688c48299ccb84e3d2b1c72c01b4e42e23021ad
Frankofe <frankofe1997@gmail.com>
2026-02-03 13:09:42 -0300

    [UPD][employee_classification]

--- dms_employee_classification/wizards/__init__.py
+++ dms_employee_classification/wizards/__init__.py
@@ -1,3 +1,5 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from . import wizard_dms_classification
+from . import wizard_dms_classification_detail
+from . import wizard_signature_control
---
+++ dms_employee_classification/wizards/wizard_signature_control.py
@@ -0,0 +1,24 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class WizardSignatureControl(models.TransientModel):
+    _name = "wizard.signature.control"
+    _description = "Wizard for Signature Control Report"
+
+    date_from = fields.Date(
+        string="Fecha Inicial",
+        required=True,
+    )
+    date_to = fields.Date(
+        string="Fecha Final",
+        required=True,
+    )
+
+    def action_export_excel(self):
+        """Export signature control report to Excel."""
+        self.ensure_one()
+        return self.env.ref(
+            'dms_employee_classification.signature_control_report'
+        ).report_action(self)
