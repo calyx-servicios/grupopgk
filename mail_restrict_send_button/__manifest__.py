79f7cde6472500bbf527356c96030818474de6ed
Martin Bravo <mbravo@calyxservicios.com.ar>
2026-02-13 12:31:44 -0300

    [ADD][mail_restrict_send_button]

---
+++ mail_restrict_send_button/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2021 Open Source Integrators
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Mail Restrict Send Button",
+    "summary": "Security for Send Message Button on Chatter Area",
+    "version": "15.0.1.0.1",
+    "category": "Social Network",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/social",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "installable": True,
+    "maintainer": ["dreispt"],
+    "development_status": "Beta",
+    "data": ["security/res_groups.xml"],
+    "assets": {
+        "web.assets_backend": [
+            "mail_restrict_send_button/static/src/js/chatter.esm.js",
+        ],
+        "web.assets_qweb": [
+            "mail_restrict_send_button/static/src/xml/chatter.xml",
+        ],
+    },
+}
