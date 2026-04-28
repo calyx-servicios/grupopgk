ea1fe51f44df40200738a41053d92e16ac72f7f0
Frankofe <frankofe1997@gmail.com>
2026-04-28 12:22:35 -0300

    [ADD][contacts_rrhh_filter]

---
+++ contacts_rrhh_filter/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "Contacts RRHH Filter",
+    "summary": (
+        "Restringe la vista de contactos a la propia ficha del usuario "
+        "con grupo RRHH sin ser administrador."
+    ),
+    "author": "Grupo PGK",
+    "maintainers": ["Frankofe"],
+    "website": "https://www.grupopgk.com.ar/",
+    "license": "AGPL-3",
+    "category": "Human Resources",
+    "version": "15.0.1.0.0",
+    "application": False,
+    "installable": True,
+    "auto_install": False,
+    "depends": [
+        "contacts",
+        "hr",
+        "calendar",
+    ],
+    "data": [
+        "views/calendar_event_views.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "contacts_rrhh_filter/static/src/js/calendar_model_patch.js",
+        ],
+    },
+}
