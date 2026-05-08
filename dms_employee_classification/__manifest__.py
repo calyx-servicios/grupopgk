ddf2ad6a8dcac8e9e92133050b462cdeb3ea4c50
Frankofe <frankofe1997@gmail.com>
2026-05-08 15:25:02 -0300

    [1536][FIX][dms_employee_classification]

--- dms_employee_classification/__manifest__.py
+++ dms_employee_classification/__manifest__.py
@@ -7,7 +7,7 @@
     "website": "https://www.grupopgk.com.ar/",
     "license": "AGPL-3",
     "category": "Document Management",
-    "version": "15.0.1.6.0",
+    "version": "15.0.1.6.1",
     "application": False,
     "installable": True,
     "auto_install": False,
