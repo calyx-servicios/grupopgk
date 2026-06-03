cc8c828477607f6a9a35168fdf7a12f03ba0b0bd
Martin Bravo <mbravo@calyxservicios.com.ar>
2026-06-03 11:33:02 -0300

    [UPD][dms_employee_classification][15.0.1.6.3]

--- dms_employee_classification/__manifest__.py
+++ dms_employee_classification/__manifest__.py
@@ -7,7 +7,7 @@
     "website": "https://www.grupopgk.com.ar/",
     "license": "AGPL-3",
     "category": "Document Management",
-    "version": "15.0.1.6.1",
+    "version": "15.0.1.6.3",
     "application": False,
     "installable": True,
     "auto_install": False,
