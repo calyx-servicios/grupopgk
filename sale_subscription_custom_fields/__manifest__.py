1d126c384fab02efc1a29aea19ee384ec728ad4a
Martin Bravo <mbravo@calyxservicios.com.ar>
2026-07-30 14:49:02 -0300

    [UPD][sale_subscription_custom_fields][15.0.3.0.1]

--- sale_subscription_custom_fields/__manifest__.py
+++ sale_subscription_custom_fields/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Sign",
-    "version": "15.0.3.0.2",
+    "version": "15.0.3.0.1",
     "application": False,
     "installable": True,
     "depends": [
