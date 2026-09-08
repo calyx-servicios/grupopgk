79dacca06e95acee8691197ce425cc4295384e14
Frankofe <frankofe1997@gmail.com>
2026-09-08 14:18:26 -0300

    [2180][consolidation_report]

---
+++ account_analytic_currency/report/__init__.py
@@ -0,0 +1,1 @@
+from . import consolidated_currency_report
---
+++ account_analytic_currency/report/consolidated_currency_report.py
@@ -0,0 +1,32 @@
+from odoo import models, _
+
+class ConsolidatedCurrencyReportXls(models.AbstractModel):
+    _name = "report.account_analytic_currency.consolidated_report_xls"
+    _inherit = "report.report_xlsx.abstract"
+    _description= "Consolidated Currency Report XLs"
+    
+    def generate_xlsx_report(self, workbook, data, obj):
+        sheet = workbook.add_worksheet(_('Consolidated Report'))
+        bold = workbook.add_format({
+            'bold': True,
+            'align': 'center',
+        })
+        columns = [
+            ('A', 50, _('Descriptions')),
+            ('B', 18, _('Account Analytic')),
+            ('C', 18, _('Company')),
+            ('D', 18, _('Amount')),
+            ('E', 18, _('Currency'))
+        ]
+        for col, width, label in columns:
+            sheet.set_column(f'{col}:{col}', width)
+            sheet.write(f'{col}1', label, bold)
+        row = 2
+        for record in data['vals']:
+            sheet.write(f'A{row}', record.get('description', ''))
+            sheet.write(f'B{row}', record.get('account_id', ''))
+            sheet.write(f'C{row}', record.get('company', ''))
+            sheet.write(f'D{row}', record.get('amount', 0.0))
+            sheet.write(f'E{row}', record.get('currency', []))
+            
+            row += 1
