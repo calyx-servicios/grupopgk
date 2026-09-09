082c9bd73cec5b064b5e87812bc1e07fabd442aa
Martin Bravo <mbravo@calyxservicios.com.ar>
2026-09-09 15:10:16 -0300

    [ADD][bi_email_cc_scope_fix]

---
+++ bi_email_cc_scope_fix/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "BI Email CC Scope Fix",
+    "summary": "Corrige el scoping del wizard usado para CC/BCC/reply-to en bi_email_cc",
+    "author": "Calyx Servicios S.A.",
+    "website": "https://odoo.calyx-cloud.com.ar/",
+    "license": "LGPL-3",
+    "category": "Accounting",
+    "version": "15.0.1.0.0",
+    "application": False,
+    "auto_install": False,
+    "installable": True,
+    "depends": ["bi_email_cc"],
+    "data": [],
+}
