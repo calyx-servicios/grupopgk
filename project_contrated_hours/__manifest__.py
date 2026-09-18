738f3ccfb2715a11fe6a6de280baa1570ccc4dfd
Martin Bravo <mbravo@calyxservicios.com.ar>
2026-09-18 17:03:29 -0300

    [UPD][project_contrated_hours][15.0.3.3.10]

--- project_contrated_hours/__manifest__.py
+++ project_contrated_hours/__manifest__.py
@@ -8,7 +8,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Technical Settings",
-    "version": "15.0.3.3.9",
+    "version": "15.0.3.3.10",
     "application": False,
     "installable": True,
     "depends": [
