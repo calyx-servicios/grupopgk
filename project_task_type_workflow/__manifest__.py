e9722c9468900395e26de4601f2d62656a9bb983
Martin Bravo <mbravo@calyxservicios.com.ar>
2026-09-25 12:41:39 -0300

    [UPD][project_task_type_workflow][15.0.1.0.2]

--- project_task_type_workflow/__manifest__.py
+++ project_task_type_workflow/__manifest__.py
@@ -9,7 +9,7 @@
     "website": "https://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Project",
-    "version": "15.0.1.0.1",
+    "version": "15.0.1.0.2",
     "development_status": "Production/Stable",
     "application": False,
     "installable": True,
